6bbf612b5d69899fc21d70e16ea705d8b8ead462
dennyjacob500 <!Cmyacob1>
2016-08-31 23:20:31 -0400

    Email Validation

--- js/script.js
+++ js/script.js
@@ -2,6 +2,9 @@ $(function() {
   jQuery.validator.addMethod("lettersonly", function(value, element) {
 return this.optional(element) || /^[a-z\s]+$/i.test(value);
 }, "Only alphabetical characters");
+    jQuery.validator.addMethod("validmail", function(value, element) {
+return this.optional(element) ||  /^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i.test(value);
+}, "Invalid E-Mail");
   // Initialize form validation on the registration form.
   // It has the name attribute "registration"
 
@@ -21,7 +24,8 @@ return this.optional(element) || /^[a-z\s]+$/i.test(value);
         maxlength: 56,
         // Specify that email should be validated
         // by the built-in "email" rule
-        email: true
+        email: true,
+        validmail: true
       },
       inputPassword: {
         required: true,
